cdf9d3503e6c0e40171150c2d0794aeadc95a8d3
Jalal Hosseini <jalalhosseiny@gmail.com>
2022-11-06 18:38:16 +0000

    add parse-message functionality and refactor version

--- cmd/version.go
+++ cmd/version.go
@@ -8,6 +8,16 @@ import (
 	"github.com/spf13/cobra"
 	"github.com/spf13/viper"
 	"os"
+	"regexp"
+)
+
+type bumpOperation int
+
+const (
+	_ bumpOperation = iota
+	major
+	minor
+	patch
 )
 
 var versionCmd = &cobra.Command{
@@ -19,6 +29,8 @@ var versionCmd = &cobra.Command{
 		cobra.CheckErr(err)
 
 		verPrefix := viper.GetString("version_prefix")
+		withPrefix, err := cmd.Flags().GetBool("with-prefix")
+		cobra.CheckErr(err)
 
 		releaseFile := fmt.Sprintf("%s/%s", cwd, ReleaseFile)
 
@@ -34,42 +46,44 @@ var versionCmd = &cobra.Command{
 
 		releaseData, err := internal.ParseReleaseFile(releaseFile)
 		cobra.CheckErr(err)
-		version, err := semver.NewVersion(releaseData.Version)
-		cobra.CheckErr(err)
 
-		var newVer = *version // set new ver to current version so, we print the current version in case of no bump option
-
-		major, err := cmd.Flags().GetBool("major")
+		maj, err := cmd.Flags().GetBool("major")
 		cobra.CheckErr(err)
-		if major {
-			newVer = version.IncMajor()
+		if maj {
+			err = bumpVersion(releaseData, releaseFile, major, withPrefix)
+			cobra.CheckErr(err)
+			return
 		}
 
-		minor, err := cmd.Flags().GetBool("minor")
+		m, err := cmd.Flags().GetBool("minor")
 		cobra.CheckErr(err)
-		if minor {
-			newVer = version.IncMinor()
+		if m {
+			err = bumpVersion(releaseData, releaseFile, minor, withPrefix)
+			cobra.CheckErr(err)
+			return
 		}
 
-		patch, err := cmd.Flags().GetBool("patch")
+		p, err := cmd.Flags().GetBool("patch")
 		cobra.CheckErr(err)
-		if patch {
-			newVer = version.IncPatch()
+		if p {
+			err = bumpVersion(releaseData, releaseFile, patch, withPrefix)
+			cobra.CheckErr(err)
+			return
 		}
 
-		releaseData.Version = newVer.String()
-		err = releaseData.Write(releaseFile)
+		// the logic with parse-message: if it's empty or option not provided then "just print current version"
+		// if message is provided but can't be parsed then, don't produce any output and exit successfully
+		message, err := cmd.Flags().GetString("parse-message")
 		cobra.CheckErr(err)
-
-		withPrefix, err := cmd.Flags().GetBool("with-prefix")
-		cobra.CheckErr(err)
-
-		if withPrefix {
-			fmt.Printf("%s%s\n", releaseData.Prefix, releaseData.Version)
-		} else {
-			fmt.Println(releaseData.Version)
+		if message == "" {
+			printVersion(releaseData, withPrefix)
+			return
 		}
-
+		op, err := parseMessage(message)
+		if err != nil {
+			return // Do not produce any output
+		}
+		err = bumpVersion(releaseData, releaseFile, op, withPrefix)
 		cobra.CheckErr(err)
 	},
 }
@@ -81,7 +95,65 @@ func init() {
 	versionCmd.PersistentFlags().Bool("major", false, "bump major version")
 	versionCmd.PersistentFlags().Bool("minor", false, "bump minor version")
 	versionCmd.PersistentFlags().Bool("patch", false, "bump patch version")
-	versionCmd.MarkFlagsMutuallyExclusive("major", "minor", "patch")
+	versionCmd.PersistentFlags().String("parse-message", "", "parse a message (usually commit message) to decide the bump operation. If parsing fails then there wont' be any output. The pattern is `message.startsWith([release:+<bump>])` for example \"[release:+major]\"")
+	versionCmd.MarkFlagsMutuallyExclusive("major", "minor", "patch", "parse-message")
 
 	versionCmd.PersistentFlags().Bool("with-prefix", false, "print the version on stdout with the prefix; useful for git tags")
+}
+
+func printVersion(rd internal.ReleaseData, withPrefix bool) {
+	if withPrefix {
+		fmt.Printf("%s%s\n", rd.Prefix, rd.Version)
+	} else {
+		fmt.Println(rd.Version)
+	}
+}
+
+func bumpVersion(releaseData internal.ReleaseData, releaseFile string, op bumpOperation, withPrefix bool) error {
+	version, err := semver.NewVersion(releaseData.Version)
+	if err != nil {
+		return err
+	}
+
+	var newVer = *version
+	switch op {
+	case major:
+		newVer = version.IncMajor()
+	case minor:
+		newVer = version.IncMinor()
+	case patch:
+		newVer = version.IncPatch()
+	default:
+		return fmt.Errorf("NotSupported")
+	}
+
+	releaseData.Version = newVer.String()
+	if err = releaseData.Write(releaseFile); err != nil {
+		return err
+	}
+
+	printVersion(releaseData, withPrefix)
+
+	return nil
+}
+
+const messageRegex = `^\[release:\+(major|minor|patch)\]`
+
+func parseMessage(msg string) (bumpOperation, error) {
+	r := regexp.MustCompile(messageRegex)
+	s := r.FindStringSubmatch(msg)
+	if s == nil {
+		return -1, fmt.Errorf("ParseError")
+	}
+	op := s[1]
+	switch op {
+	case "major":
+		return major, nil
+	case "minor":
+		return minor, nil
+	case "patch":
+		return patch, nil
+	default:
+		return -1, fmt.Errorf("ParseError")
+	}
 }
